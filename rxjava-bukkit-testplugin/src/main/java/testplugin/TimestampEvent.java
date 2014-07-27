74d90ac5c0294d640a304a5bef24f0000137533b
Ryan Michela <deltahat@gmail.com>
2014-07-26 19:27:15 -0700

    Added TimestampEvent

---
+++ rxjava-bukkit-testplugin/src/main/java/testplugin/TimestampEvent.java
@@ -0,0 +1,12 @@
+package testplugin;
+
+import org.bukkit.event.Event;
+import java.util.Date;
+
+/**
+ * Copyright 2014 Ryan Michela
+ */
+public class TimestampEvent {
+    public Event event;
+    public Date eventTimestamp;
+}
